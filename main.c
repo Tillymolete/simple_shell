9f1fc961ac4bc64a1844144d626982c79d0babee
MbaliFelecia <ramakgashambali@gmail.com>
2023-07-26 06:07:53 -0700

    HOT FIX

--- main.c
+++ main.c
@@ -11,41 +11,41 @@
 
 int main(int argc, char **argv, char **env)
 {
-    ssize_t numchar = 0;
-    size_t buf = 0;
-    char *inchar = NULL, *nm = argv[0], **tokens = NULL;
-    char *ps1 = NULL;
+	ssize_t numchar = 0;
+	size_t buf = 0;
+	char *inchar = NULL, *nm = argv[0], **tokens = NULL;
+	char *ps1 = NULL;
 
-    /*Get the PS1 environment variable, or set a default prompt*/
-    ps1 = get_ps1();
-    
-    if (argc == 1)
-    {
-	    while (1)
-	    {
-		    printf("%s", ps1);
-		    
-		    numchar = getline(&inchar, &buf, stdin);
-		    if (numchar < 0 || numchar == EOF)
-			    break;
-		    /* Remove the trailing newline character*/
-		    if (inchar[numchar - 1] == '\n')
-		    {
-			    inchar[numchar - 1] = '\0';
-			    numchar--;
-		    }
-		    
-		    /* Tokenize the input command*/
-		    tokens = tokenize(inchar, " ");
-		    /* Execute the command*/
-		    execu(tokens, env, nm);
-		    /* Free the memory used for tokens*/
-		    free_tokens(tokens);
-	    }
-    }
-    /*Free the memory used for the input buffer*/
-    free(inchar);
-    return (0);
+	/*Get the PS1 environment variable, or set a default prompt*/
+	ps1 = get_ps1();
+
+	if (argc == 1)
+	{
+		while (1)
+		{
+			printf("%s", ps1);
+
+			numchar = getline(&inchar, &buf, stdin);
+			if (numchar < 0 || numchar == EOF)
+				break;
+
+			/* Remove the trailing newline character*/
+			if (inchar[numchar - 1] == '\n')
+			{
+				inchar[numchar - 1] = '\0';
+				numchar--;
+			}
+			/* Tokenize the input command*/
+			tokens = tokenize(inchar, " ");
+			/* Execute the command*/
+			execu(tokens, env, nm);
+			/* Free the memory used for tokens*/
+			free_tokens(tokens);
+		}
+	}
+	/*Free the memory used for the input buffer*/
+	free(inchar);
+	return (0);
 }
 
 /**
@@ -73,9 +73,10 @@ void free_tokens(char **tokens)
  */
 char *get_ps1()
 {
-    char *ps1 = getenv("PS1");
-    if (ps1 == NULL)
-        /*Default prompt if PS1 is not set*/
-        ps1 = "#cisfun$ ";
-    return (ps1);
+	char *ps1 = getenv("PS1");
+
+	if (ps1 == NULL)
+		/*Default prompt if PS1 is not set*/
+		ps1 = "#cisfun$ ";
+	return (ps1);
 }
--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -15,13 +16,13 @@
 char **tokenize(char *input, const char *delimiters);
 void free_tokens(char **tokens);
 char *get_ps1();
-/*int token(char *string, char delim[]);
-char **token1(char **argv, char *str, char delim[]);
-char ** mem(char **str, size_t n);
-char *mem1(char *str, size_t n);*/
 void execu(char **tokens, char **env, char *nm);
 void free_mem(char **str1, char *str2, char *str3);
 char *create_path(const char *inp);
 void print_envir(char **env, char **argv);
 char *get_line();
+int _strlen(const char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strdup(char *str);
+
 #endif
