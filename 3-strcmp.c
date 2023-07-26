6b0e82d19651ca61f2d9440da154c173ec4dc60e
MbaliFelecia <ramakgashambali@gmail.com>
2023-07-26 14:04:28 -0700

    file removed

---
+++ 3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: the string comparision
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
+
+/*{
+	size_t i;
+	int diff;
+
+	for (i = 0; i < n; i++)
+	{
+		diff = s1[i] - s2[i];
+		if (diff != 0)
+			break;
+	}
+	return (diff);
+}*/
